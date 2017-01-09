292a8536abbed859755dbd369d46c77fb35bf93a
Yoshiyuki Mineo <yoshiyuki.mineo@gmail.com>
2017-01-09 16:20:17 +0900

    Optimize the performance of context

--- context.go
+++ context.go
@@ -2,6 +2,7 @@ package jsm
 
 import (
 	"context"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -15,28 +16,51 @@ const (
 	keyResult
 )
 
+type machineContext map[machineContextKey]interface{}
+
+func (mc *machineContext) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (mc *machineContext) Done() <-chan struct{} {
+	return nil
+}
+
+func (mc *machineContext) Err() error {
+	return nil
+}
+
+func (mc *machineContext) Value(key interface{}) interface{} {
+	switch key.(type) {
+	case machineContextKey:
+		return (*mc)[key.(machineContextKey)]
+	default:
+		return nil
+	}
+}
+
 func newMachineContext(m *machine) context.Context {
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, keyPC, Address(m.PC))
-	ctx = context.WithValue(ctx, keyHeap, Heap(m.Heap))
-	ctx = context.WithValue(ctx, keyStack, Stack(m.Stack))
-	ctx = context.WithValue(ctx, keyResult, new(interface{}))
-	return ctx
+	return &machineContext{
+		keyPC:     m.PC,
+		keyHeap:   m.Heap,
+		keyStack:  m.Stack,
+		keyResult: new(interface{}),
+	}
 }
 
 // GetPC retrieves the program counter from Context.
 func GetPC(ctx context.Context) Address {
-	return ctx.Value(keyPC).(Address)
+	return (*ctx.(*machineContext))[keyPC].(*address)
 }
 
 // GetHeap retrieves Heap from Context.
 func GetHeap(ctx context.Context) Heap {
-	return ctx.Value(keyHeap).(Heap)
+	return (*ctx.(*machineContext))[keyHeap].(*heap)
 }
 
 // GetStack retrieves Stack from Context.
 func GetStack(ctx context.Context) Stack {
-	return ctx.Value(keyStack).(Stack)
+	return (*ctx.(*machineContext))[keyStack].(*stack)
 }
 
 // GetFrame retrieves the current Frame from Context.
@@ -73,11 +97,11 @@ func GetLocals(ctx context.Context) (Heap, error) {
 }
 
 func getResult(ctx context.Context) interface{} {
-	return *ctx.Value(keyResult).(*interface{})
+	return *(*ctx.(*machineContext))[keyResult].(*interface{})
 }
 
 func setResult(ctx context.Context, res interface{}) {
-	r := ctx.Value(keyResult).(*interface{})
+	r := (*ctx.(*machineContext))[keyResult].(*interface{})
 	*r = res
 }
 
@@ -88,24 +112,47 @@ const (
 	keyMnemonic
 )
 
+type programContext map[programContextKey]interface{}
+
+func (pc *programContext) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (pc *programContext) Done() <-chan struct{} {
+	return nil
+}
+
+func (pc *programContext) Err() error {
+	return nil
+}
+
+func (pc *programContext) Value(key interface{}) interface{} {
+	switch key.(type) {
+	case programContextKey:
+		return (*pc)[key.(programContextKey)]
+	default:
+		return nil
+	}
+}
+
 func newProgramContext() context.Context {
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, keyLabels, map[string]int{})
-	ctx = context.WithValue(ctx, keyMnemonic, new(Mnemonic))
-	return ctx
+	return &programContext{
+		keyLabels:   map[string]int{},
+		keyMnemonic: new(Mnemonic),
+	}
 }
 
 // GetLabels retrieves the program labels from Context.
 func GetLabels(ctx context.Context) map[string]int {
-	return ctx.Value(keyLabels).(map[string]int)
+	return (*ctx.(*programContext))[keyLabels].(map[string]int)
 }
 
 // GetMnemonic retrieves the currently preprocessed mnemonic from Context.
 func GetMnemonic(ctx context.Context) Mnemonic {
-	return *ctx.Value(keyMnemonic).(*Mnemonic)
+	return *(*ctx.(*programContext))[keyMnemonic].(*Mnemonic)
 }
 
 func setMnemonic(ctx context.Context, mnemonic Mnemonic) {
-	m := ctx.Value(keyMnemonic).(*Mnemonic)
+	m := (*ctx.(*programContext))[keyMnemonic].(*Mnemonic)
 	*m = mnemonic
 }
---
+++ context_test.go
@@ -0,0 +1,31 @@
+package jsm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMachineContext(t *testing.T) {
+	assert := assert.New(t)
+
+	mc := newMachineContext(newMachine())
+	assert.Equal(mc.Value(keyPC), GetPC(mc))
+	assert.Equal(mc.Value(keyHeap), GetHeap(mc))
+	assert.Equal(mc.Value(keyStack), GetStack(mc))
+
+	setResult(mc, 3)
+	assert.Equal(3, getResult(mc))
+	assert.Equal(3, *mc.Value(keyResult).(*interface{}))
+}
+
+func TestProgramContext(t *testing.T) {
+	assert := assert.New(t)
+
+	pc := newProgramContext()
+	assert.Equal(pc.Value(keyLabels), GetLabels(pc))
+
+	setMnemonic(pc, MnemonicAdd)
+	assert.Equal(string(MnemonicAdd), string(GetMnemonic(pc)))
+	assert.Equal(string(MnemonicAdd), string(*pc.Value(keyMnemonic).(*Mnemonic)))
+}
